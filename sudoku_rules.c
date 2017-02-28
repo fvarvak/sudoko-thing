1bc39d3d8797f6330635aa5453e10d84a9f5b7cd
Phillip <fvarvak@gmail.com>
2017-02-27 21:49:44 -0500

    implemented sudoku_get_box

--- sudoku_rules.c
+++ sudoku_rules.c
@@ -23,3 +23,17 @@ void sudoku_get_col(SudokuBoard *board, int colnum, SudokuSet *col)
   for (i = 0; i < SNUMSETS; i++)
     col->cells[i] = board->cells[i*SSETSIZE+colnum];
 } 
+
+void sudoku_get_box(SudokuBoard *board, int boxnum, SudokuSet *box)
+{
+  int i;
+  assert(board && box && boxnum >=0 && boxnum < SNUMSETS);
+  for (i = 0; i < SNUMSET; i++)
+  {
+    int startrow = (boxnum / 3) * 3;
+	int startcol = (boxnum % 3) * 3;
+	int row = startrow + i / 3;
+	int col = startcol + i % 3;
+    box->cells[i] = board->cells[row * SSETSIZE + col];
+  }
+}
